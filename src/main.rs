7564f852d027c59bf62ea9f7e2ec096ac9c38e34
Thomas McKernan <tmeaglei@gmail.com>
2022-04-05 22:58:00 -0700

    init

---
+++ src/main.rs
@@ -0,0 +1,29 @@
+use crate::args::args::{Cli, Commands};
+use clap::Parser;
+use commands::punch;
+use log::debug;
+use simplelog::{ColorChoice, CombinedLogger, Config, TermLogger, TerminalMode};
+
+mod args;
+mod commands;
+
+fn main() {
+    let args: Cli = Cli::parse();
+    let command = args.command;
+    let log_level = args.verbose.log_level_filter();
+    CombinedLogger::init(vec![TermLogger::new(
+        log_level,
+        Config::default(),
+        TerminalMode::Mixed,
+        ColorChoice::Auto,
+    )])
+    .expect("Logger failed to instantiate");
+
+    debug!("Got Command {:?}", command);
+
+    match command {
+        Commands::Punch(p) => {
+            punch::punch(p);
+        }
+    };
+}
